085090f9ceff15fd16c523208de55ac6e747916d
Lucien Cartier-Tilet <lucien@phundrak.com>
2022-08-07 21:52:48 +0200

    initial commit

---
+++ mdc-mode.el
@@ -0,0 +1,37 @@
+;;; mdc-mode.el --- Major mode for Nuxt’s MDC markdown flavor  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Lucien Cartier-Tilet
+
+;; Author: Lucien Cartier-Tilet <lucien@phundrak.com>
+;; Maintainer: Lucien Cartier-Tilet <lucien@phundrak.com>
+;; URL: https://labs.phundrak.com/phundrak/mdc-mode
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "26.1") (markdown-mode "2"))
+;; Keywords: languages, convenience, faces
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Major mode for Nuxt’s MDC markdown flavor
+;;
+
+;;; Code:
+
+(require 'markdown-mode)
+
+(provide 'mdc-mode)
+;;; mdc-mode.el ends here
